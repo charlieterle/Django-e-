7d8aa7284997cf2d73ffbc5f7b21f53b961575e3
charlieterle <cdieterle33@gmail.com>
2024-12-28 14:39:43 +0900

    Start work on models for the catalog app

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "catalog.apps.CatalogConfig",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# カスタムユーザーモデルを登録
+AUTH_USER_MODEL = 'catalog.User'
